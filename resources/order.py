a569b302303b1afec0258fa689b374a6b14a60df
IanGichane <gichaneian@gmail.com>
2024-01-26 00:07:33 +0300

    products

--- resources/order.py
+++ resources/order.py
@@ -1,1 +1,17 @@
-#
+from flask_restful import Resource, fields, marshal_with, reqparse
+
+from models import OrderModel, db
+
+
+class Orders(Resource):
+    def post(self):
+        def get(self, id=None):
+            if id:
+                orders = OrderModel.query.filter_by(id=id).first()
+                return property
+            else:
+                orders = OrderModel.query.all()
+
+            return properties
+
+      
